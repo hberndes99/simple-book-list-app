de4196fc32165f1ba097c2f14bd6c6e05e19581e
harriette <harrietteb@gmail.com>
2021-03-19 17:39:00 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// class Books : represents a book
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn
+    }
+}
+
+// class UI to handle UI tasks (user interface)
+
+
+// store class to take care of storage 
+
+
+// event display books
+
+
+//event add book 
+
+
+// event remove book 
